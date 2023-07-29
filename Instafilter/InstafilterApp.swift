91e1d3b28a4df731fffc17dc4408f1f9ee6e5878
LuiArm <Luisarmendariz24@icloud.com>
2023-07-29 14:47:59 -0600

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by luis armendariz on 7/29/23.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
